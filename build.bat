7714f6c509677fef72c62ac0ae874bfa81b27ebe
Reece Bennett <reece.bennett1@gmail.com>
2017-06-06 13:10:23 +0100

    Initial commit

---
+++ build.bat
@@ -0,0 +1,9 @@
+@echo off
+call roots compile -e production
+cd public
+git init
+git add .
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a:%%b)
+git commit -m "Site updated: %mydate% %mytime%"
+git push git@github.com:PhyscoKillerMonkey/website-test.git --force
