e8a35f3769b9569080990b94d6d9a571e8d1a59f
Reece Bennett <reece.bennett1@gmail.com>
2017-06-06 15:14:55 +0100

    Correct the repo url in build.bat

--- build.bat
+++ build.bat
@@ -6,4 +6,4 @@ git add .
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a:%%b)
 git commit -m "Site updated: %mydate% %mytime%"
-git push git@github.com:PhyscoKillerMonkey/website-test.git --force
+git push git@github.com:PhyscoKillerMonkey/physcokillermonkey.github.io.git --force
