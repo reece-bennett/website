2f221e2459bc8fd10cc7ae58ecd22b70572e455f
Reece Bennett <reece.bennett1@gmail.com>
2018-09-28 14:35:26 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ call roots compile -e production
 cd public
 git init
 git add .
-For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%b-%%a)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a:%%b)
 git commit -m "Site updated: %mydate% %mytime%"
-git push git@github.com:PhyscoKillerMonkey/physcokillermonkey.github.io.git --force
+git push --set-upstream git@github.com:PhyscoKillerMonkey/physcokillermonkey.github.io.git master --force
