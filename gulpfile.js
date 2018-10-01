a32be68aaf9b8c6f8480bd9f0e0abd93b09926bc
Reece Bennett <reece.bennett1@gmail.com>
2018-10-01 18:41:34 +0100

    Add gulp-stylus

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,31 @@
-var gulp = require("gulp");
-var pug = require("gulp-pug");
+const gulp = require("gulp");
+const pug = require("gulp-pug");
+const stylus = require("gulp-stylus");
 
 const paths = {
+  assets: {
+    src: "assets/**/*",
+    dest: "dist"
+  },
+  css: {
+    src: "css/!(_)*.styl",
+    dest: "dist/css"
+  },
   views: {
-    src: "views/*.pug",
+    src: "views/!(_)*.pug",
     dest: "dist"
   }
+}
+
+function assets() {
+  return gulp.src(paths.assets.src)
+    .pipe(gulp.dest(paths.assets.dest));
+}
+
+function css() {
+  return gulp.src(paths.css.src)
+    .pipe(stylus())
+    .pipe(gulp.dest(paths.css.dest));
 }
 
 function views() {
@@ -17,10 +37,12 @@ function views() {
 }
 
 function watch() {
+  gulp.watch(paths.assets.src, assets);
+  gulp.watch(paths.css.src, css);
   gulp.watch(paths.views.src, views);
 }
 
-const build = gulp.series(views);
+const build = gulp.series(views, css, assets);
 
 exports.build = build;
 exports.default = build;
